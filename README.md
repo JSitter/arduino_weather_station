407675bb491e3ce4807599e2de63c65958a6cf05
Justin Sitter <justin@linux-ikdp.attlocal.net>
2019-01-05 11:02:17 -0800

    Initial commit. Yay.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple Weather Station
+
+## Bare Bones
+This is a a bare bones example that uses external libraries to drive a display and barometer.
---
+++ weather-station.ino
@@ -0,0 +1,206 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <dht.h>
+#include <SFE_BMP180.h>
+
+dht DHT;
+SFE_BMP180 pressure;
+
+#define DHT11_PIN 13
+
+#define OLED_RESET 4
+#define ALTITUDE 9 // Altitude
+Adafruit_SSD1306 display(OLED_RESET);
+
+
+int temp = 0;
+int humidity = 0;
+double T,P,p0,a;
+char status;
+
+
+
+
+
+
+void setup()   {                
+  Serial.begin(9600);
+  pressure.begin();
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  // init done
+  display.clearDisplay();
+
+  
+  testdrawtriangle();
+  display.display();
+  delay(250);
+  display.clearDisplay();
+  
+  
+  
+}
+
+
+void loop() {
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    // Wait for the measurement to complete:
+    delay(status);
+
+    // Retrieve the completed temperature measurement:
+    // Note that the measurement is stored in the variable T.
+    // Function returns 1 if successful, 0 if failure.
+
+    status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      // Print out the measurement:
+      Serial.print("temperature: ");
+      Serial.print(T,2);
+      Serial.print(" deg C, ");
+      Serial.print((9.0/5.0)*T+32.0,2);
+      Serial.println(" deg F");
+      
+      // Start a pressure measurement:
+      // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
+      // If request is successful, the number of ms to wait is returned.
+      // If request is unsuccessful, 0 is returned.
+
+      status = pressure.startPressure(3);
+      if (status != 0)
+      {
+        // Wait for the measurement to complete:
+        delay(status);
+
+        // Retrieve the completed pressure measurement:
+        // Note that the measurement is stored in the variable P.
+        // Note also that the function requires the previous temperature measurement (T).
+        // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
+        // Function returns 1 if successful, 0 if failure.
+
+        status = pressure.getPressure(P,T);
+        if (status != 0)
+        {
+          // Print out the measurement:
+          Serial.print("absolute pressure: ");
+          Serial.print(P,2);
+          Serial.print(" mb, ");
+          Serial.print(P*0.0295333727,2);
+          Serial.println(" inHg");
+
+          // The pressure sensor returns abolute pressure, which varies with altitude.
+          // To remove the effects of altitude, use the sealevel function and your current altitude.
+          // This number is commonly used in weather reports.
+          // Parameters: P = absolute pressure in mb, ALTITUDE = current altitude in m.
+          // Result: p0 = sea-level compensated pressure in mb
+
+          p0 = pressure.sealevel(P,ALTITUDE); // we're at 1655 meters (Boulder, CO)
+          Serial.print("relative (sea-level) pressure: ");
+          Serial.print(p0,2);
+          Serial.print(" mb, ");
+          Serial.print(p0*0.0295333727,2);
+          Serial.println(" inHg");
+
+          // On the other hand, if you want to determine your altitude from the pressure reading,
+          // use the altitude function along with a baseline pressure (sea-level or other).
+          // Parameters: P = absolute pressure in mb, p0 = baseline pressure in mb.
+          // Result: a = altitude in m.
+
+          a = pressure.altitude(P,p0);
+          Serial.print("computed altitude: ");
+          Serial.print(a,0);
+          Serial.print(" meters, ");
+          Serial.print(a*3.28084,0);
+          Serial.println(" feet");
+        }
+        else Serial.println("error retrieving pressure measurement\n");
+      }
+      else Serial.println("error starting pressure measurement\n");
+    }
+    else Serial.println("error retrieving temperature measurement\n");
+  }
+  else Serial.println("error starting temperature measurement\n");
+
+ 
+
+  
+  setValues();
+  scrollTemp();
+}
+
+
+
+
+void testdrawtriangle(void) {
+  for (int16_t i=0; i<min(display.width(),display.height())/2; i+=5) {
+    display.drawTriangle(display.width()/2, display.height()/2-i,
+                     display.width()/2-i, display.height()/2+i,
+                     display.width()/2+i, display.height()/2+i, WHITE);
+    display.display();
+    delay(1);
+  }
+}
+
+
+
+
+
+void testscrolltext(void) {
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(10,0);
+  display.clearDisplay();
+  display.println("Bearghost!");
+  display.display();
+  delay(1);
+ 
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+  display.startscrollleft(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);    
+  display.startscrolldiagright(0x00, 0x07);
+  delay(2000);
+  display.startscrolldiagleft(0x00, 0x07);
+  delay(2000);
+  display.stopscroll();
+}
+
+void scrollTemp(void){
+    display.clearDisplay();
+    display.setTextSize(3);
+    display.setTextColor(WHITE);
+    display.setCursor(10,0);
+    display.println((9.0/5.0)*T+32.0,1);
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(83,-4);
+    display.println("o");
+    display.setTextSize(1);
+    display.setCursor(10, 24);
+    display.println(humidity);
+    display.setCursor(23, 24);
+    display.println("% Humidity");
+    display.setCursor(85,24);
+    display.println(p0,2);
+    display.display();
+    delay(1);
+    display.startscrollleft(0x00, 0x0F);
+    delay(4165);
+    
+    
+  };
+
+  void setValues(void){
+    int chk = DHT.read11(DHT11_PIN);
+    temp = DHT.temperature;
+    temp = temp * 9 / 5 + 32;
+    humidity = DHT.humidity;
+    };
